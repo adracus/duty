fe39c8c088c099ad99ea3c75cf0489ed6246663d
Adracus <adracus@gmail.com>
2015-08-25 22:01:56 +0200

    Implement pattern matching

---
+++ lib/src/match.dart
@@ -0,0 +1,74 @@
+library duty.match;
+
+typedef bool Predicate(arg);
+typedef ZeroArity();
+
+class PatternMatch {
+  final Object on;
+  final List<Matcher> matchers = [];
+
+  PatternMatch(this.on);
+
+  MatcherBuilder when(condition) =>
+      new MatcherBuilder(new Condition(condition), this);
+
+  void add(Matcher matcher) => matchers.add(matcher);
+
+  call() {
+    return matchers.firstWhere((matcher) => matcher.matches(on),
+        orElse: () => throw new Exception("")).then();
+  }
+
+  run() => call();
+}
+
+class MatchingException extends Exception {
+  final on;
+  MatchingException(this.on);
+
+  String toString() => "Could not match $on";
+}
+
+class MatcherBuilder {
+  final Condition condition;
+  final PatternMatch origin;
+
+  MatcherBuilder(this.condition, this.origin);
+
+  PatternMatch then(evaluatable) {
+    final matcher = new Matcher(condition, new Evaluatable(evaluatable));
+    origin.add(matcher);
+
+    return origin;
+  }
+}
+
+class Matcher {
+  final Condition condition;
+  final Evaluatable evaluatable;
+
+  Matcher(this.condition, this.evaluatable);
+
+  bool matches(arg) => condition.matches(arg);
+  then() => evaluatable.evaluate();
+}
+
+class Condition {
+  final content;
+
+  Condition(this.content);
+
+  bool matches(arg) => content is Predicate ? content(arg) : arg == content;
+}
+
+class Evaluatable {
+  final content;
+
+  Evaluatable(this.content);
+
+  evaluate() => content is ZeroArity ? content() : content;
+}
+
+always(condition) => true;
+
+PatternMatch match(Object arg) => new PatternMatch(arg);
---
+++ test/test_match.dart
@@ -0,0 +1,70 @@
+import '../lib/src/match.dart';
+import 'package:test/test.dart';
+
+
+main() {
+  group("Match", () {
+    group("match", () {
+      group("call", () {
+        test("Successfull match", () {
+          final m = match(5)
+            .when((n) => n > 10)
+            .then(10)
+            .when((n) => n < 10)
+            .then(0);
+
+          expect(m(), equals(0));
+        });
+
+        test("Failure match", () {
+          final m = match(5)
+            .when((n) => n > 10).then(20);
+
+          expect(() => m(), throws);
+        });
+
+        test("Matching order", () {
+          final m = match(5)
+            .when(always).then(10)
+            .when((n) => n <= 5).then(0);
+
+          expect(m(), equals(10));
+        });
+      });
+    });
+
+    group("Condition", () {
+      group("matches", () {
+        test("constant condition", () {
+          final c = new Condition(1);
+
+          expect(c.matches(1), isTrue);
+          expect(c.matches(2), isFalse);
+        });
+
+        test("predicate condition", () {
+          final c = new Condition((int test) => test.isEven);
+
+          expect(c.matches(5), isFalse);
+          expect(c.matches(6), isTrue);
+        });
+      });
+    });
+
+    group("Evaluatable", () {
+      group("evaluate", () {
+        test("constant content", () {
+          final e = new Evaluatable(10);
+
+          expect(e.evaluate(), equals(10));
+        });
+
+        test("ZeroArity content", () {
+          final e = new Evaluatable(() => 10);
+
+          expect(e.evaluate(), equals(10));
+        });
+      });
+    });
+  });
+}
